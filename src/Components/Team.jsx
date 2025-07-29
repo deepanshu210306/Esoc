f6ade97fb491d0f298538d9118def1acf3678915
deepanshu210306 <deepanshu210306@gmail.com>
2025-07-29 22:15:26 +0530

    optimised

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -54,9 +54,9 @@ const Team = () => {
         className="team-swiper"
         // Responsive breakpoints for the slider
         breakpoints={{
-          320: { slidesPerView: 1, spaceBetween: 20 },
-          768: { slidesPerView: 2, spaceBetween: 30 },
-          1024: { slidesPerView: 3, spaceBetween: 30 },
+          320: { slidesPerView: 2, spaceBetween: 20 },
+          768: { slidesPerView: 3, spaceBetween: 30 },
+          1024: { slidesPerView: 4, spaceBetween: 30 },
         }}
       >
         {teamMembers.map((member, index) => (
